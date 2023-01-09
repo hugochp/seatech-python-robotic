98cd5bf977c0d247c7bd8018a7006980b7e5d7ca
hugochp <tatayoyoh@mymail>
2023-01-09 12:01:28 +0100

    exo 2 second commit

--- seatech-python-object-and-robotic-exo-main/01-python_oriente_objet/exo2_starter_template.py
+++ seatech-python-object-and-robotic-exo-main/01-python_oriente_objet/exo2_starter_template.py
@@ -1,24 +1,45 @@
-class Robot():
-    # Robot class content here
-    pass
+from exo1_starter_template import Robot
+
 
 class Human():   
     # Human class content here
+
+    # Un humain doit posséder un sexe attribuable à sa création
+    def __init__(self, sexe):
+        self.sexe = sexe
+
+    # Un humain doit pouvoir manger un ou plusieurs aliments
+    def eat(self, food):
+        if isinstance(food, list):
+            print("Eating multiple foods:")
+            for f in food:
+                print(f"Eating {f}")
+            else:
+                print(f"Eating {food}")
+
+    # Un humain doit pouvoir digérer ce qu'il a mangé
+    def digest(self):
+        print("Digesting food")
     pass
 
 class Cyborg(Robot, Human):   
 
+
     def __init__(self, name, sexe):   
         Robot.__init__(self, name)
         Human.__init__(self, sexe)
+        
+    def fun(self):
+        print("I'm a fun cyborg!")
 
 
 cyborg = Cyborg('Deux Ex Machina', 'M')
+cyborg.fun()
 
-print(cyborg.name, 'sexe', cyborg.sexe)
+print(cyborg.getName(), 'sexe', cyborg.sexe)
 print('Charging battery...')
-cyborg.charge()
-cyborg.status()
+cyborg.setChargingState(True)
+cyborg.getState()
 cyborg.eat('banana')
 cyborg.eat(['coca', 'chips'])
 cyborg.digest()
