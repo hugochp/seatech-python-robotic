021ee9233b9c73114d128f109359ced121e686ce
hugochp <tatayoyoh@mymail>
2023-01-30 09:57:10 +0100

    move in 8

--- webots/controllers/my_controller/my_controller.py
+++ webots/controllers/my_controller/my_controller.py
@@ -1,3 +1,4 @@
+import math
 from controller import Robot, Motor
 
 class WholeRobotMotor(Motor) :
@@ -23,6 +24,7 @@ class RobotControl():
 
         self.__left_wheel = WholeRobotMotor('motor.left')
         self.__right_wheel = WholeRobotMotor('motor.right')
+        self.__time = 0
 
     def go_front(self, coef):
 
@@ -34,14 +36,37 @@ class RobotControl():
         self.__left_wheel.activeMotorBackward(coef)
         self.__right_wheel.activeMotorBackward(coef)
 
+    def go_left(self, coef):
+
+        self.__left_wheel.activeMotorForward(coef)
+
+    def go_right(self, coef):
+
+        self.__right_wheel.activeMotorBackward(coef)
+
+    def move_in_eight(self, A, B, coef):
+        
+        x = A * math.cos(B * self.__time)
+        y = A * math.sin(B * self.__time)
+
+        # Left wheel moves forward or backward depending on x
+        if x > 0:
+            self.__left_wheel.activeMotorForward(x * coef)
+        else:
+            self.__left_wheel.activeMotorBackward(-x * coef)
+
+        # Right wheel moves forward or backward depending on y
+        if y > 0:
+            self.__right_wheel.activeMotorForward(y * coef)
+        else:
+            self.__right_wheel.activeMotorBackward(-y * coef)
+
+        self.__time += 0.01
+
 robot = Robot()
-
 timestep = int(robot.getBasicTimeStep())
-
 robot_Control = RobotControl()
 
 while robot.step(timestep) != -1:
-
-    robot_Control.go_front(0.5)  # to move forward
+    robot_Control.move_in_eight(1, 2, 0.5) # move in eight shape
     robot.step(timestep)
-
